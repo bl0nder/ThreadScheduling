bc3cfd5d6160a5f1dc2eaa3113fb31f2d9f05137
bl0nder <goelpalaash@gmail.com>
2022-11-13 16:49:41 +0530

    .

--- threadsched.c
+++ threadsched.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+void countA() {
+    for (unsigned long i=1; i<4294967296; i++) {
+        printf("%d ", i);
+    }
+    printf("\n");
+}
+
 int main() {
     printf("Hello world\n");
     return 0;

dab677e447128614cea2ad4cb7b2bf313a47eace
bl0nder <goelpalaash@gmail.com>
2022-11-13 18:59:11 +0530

    .

--- threadsched.c
+++ threadsched.c
@@ -9,7 +9,7 @@ void countA() {
     struct timespec end;
     int startTime = clock_gettime(CLOCK_REALTIME, &start);
     for (unsigned long i=1; i<=100000; i++) {
-        continue;
+        printf("%lu ", i);
     }
     printf("\n");
     int endTime = clock_gettime(CLOCK_REALTIME, &end);

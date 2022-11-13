d97b04d4801f3de5debe1b274a6e1538be827f5b
bl0nder <goelpalaash@gmail.com>
2022-11-13 17:15:41 +0530

    test

--- threadsched.c
+++ threadsched.c
@@ -14,7 +14,7 @@ void countA() {
     printf("\n");
     int endTime = clock_gettime(CLOCK_REALTIME, &end);
 
-    long double runTime = (end.tv_sec + end.tv_nsec - start.tv_sec - start.tv_nsec) / 1000000;
+    long double runTime = (end.tv_nsec - start.tv_nsec) / 1000000;
 
     printf("Runtime of CountA = %lf\n", runTime);
 }

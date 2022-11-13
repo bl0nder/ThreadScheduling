ad4bb495a8d88636f2bbc5b1970f32a7bb18a27f
bl0nder <goelpalaash@gmail.com>
2022-11-13 17:09:32 +0530

    testing higher num

--- threadsched.c
+++ threadsched.c
@@ -8,7 +8,7 @@ void countA() {
     struct timespec start;
     struct timespec end;
     int startTime = clock_gettime(CLOCK_REALTIME, &start);
-    for (unsigned long i=1; i<=5000; i++) {
+    for (unsigned long i=1; i<=500000; i++) {
         printf("%d ", i);
     }
     printf("\n");

1bbb2f8ff73f19fc386e7b5f5c66dd2044bf4c4c
bl0nder <goelpalaash@gmail.com>
2022-11-13 19:24:37 +0530

    .

--- threadsched.c
+++ threadsched.c
@@ -76,7 +76,7 @@ int main() {
     pthread_t ThrC;
 
     // struct args* passArgs = (struct args*) malloc (sizeof(struct args));
-    int t1 = pthread_create(&ThrA, NULL, test, NULL);
+    int t1 = pthread_create(&ThrA, NULL, &test, NULL);
     pthread_join(t1, NULL);
     // free((void *) passArgs);
 

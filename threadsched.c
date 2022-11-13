f02eb5fe2095197975d775e7822ac771d371f065
bl0nder <goelpalaash@gmail.com>
2022-11-13 16:51:20 +0530

    Added countB and countC

--- threadsched.c
+++ threadsched.c
@@ -7,6 +7,20 @@ void countA() {
     printf("\n");
 }
 
+void countB() {
+    for (unsigned long i=1; i<4294967296; i++) {
+        printf("%d ", i);
+    }
+    printf("\n");
+}
+
+void countC() {
+    for (unsigned long i=1; i<4294967296; i++) {
+        printf("%d ", i);
+    }
+    printf("\n");
+}
+
 int main() {
     countA();
     return 0;

22347d7b03009a8c92856e7bb37531cfc9860302
bl0nder <goelpalaash@gmail.com>
2022-11-14 12:34:07 +0530

    .

--- threadsched.c
+++ threadsched.c
@@ -99,8 +99,8 @@ void* Thr_C(void* args) {
 
 int main() {
     
-    int a = sched_get_priority_max(SCHED_RR);
-    int b = sched_get_priority_max(SCHED_FIFO);
+    int a = sched_get_priority_min(SCHED_RR);
+    int b = sched_get_priority_min(SCHED_FIFO);
 
     printf("%d %d\n", a, b);
 

fbd1cd0f63f357a8d2966d87a8901b19c9d45b8c
bl0nder <goelpalaash@gmail.com>
2022-11-14 16:30:57 +0530

    created processShed.c

--- processSched.c
+++ processSched.c
@@ -4,3 +4,27 @@
 #include <unistd.h>
 #include <time.h>
 #include <sched.h>
+#include <sys/types.h>
+
+int main() {
+    //Creating three processes
+    pid_t p1;
+    
+    p1 = fork();
+
+    if (p1 < 0) {
+        perror("Error executing fork(): ");
+        return -1;
+    }
+
+    else if (pid == 0) {
+        execl();
+    }
+
+    pid_t p2;
+    pid_t p3;
+
+    
+
+    return 0;
+}

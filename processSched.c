d8f1f0223a645b75e455863f5f9989c3bc8ce766
bl0nder <goelpalaash@gmail.com>
2022-11-20 13:05:12 +0530

    now?

--- processSched.c
+++ processSched.c
@@ -70,26 +70,25 @@ int main() {
         return 0;
     }
 
-    else {
+    else if (p1 > 0) {
         wait(&p1);
         int endTime1 = clock_gettime(CLOCK_REALTIME, &end1);
         double runTime1 = (end1.tv_sec + 1.0e-9*end1.tv_nsec - (start1.tv_sec + 1.0e-9*start1.tv_nsec));
         printf("Runtime of Process 1 = %lfs\n", runTime1);
+    }
 
+    else if (p2 > 0) {
         wait(&p2);
         int endTime2 = clock_gettime(CLOCK_REALTIME, &end2);
         double runTime2 = (end2.tv_sec + 1.0e-9*end2.tv_nsec - (start2.tv_sec + 1.0e-9*start2.tv_nsec));
         printf("Runtime of Process 2 = %lfs\n", runTime2);
-
+    }
         
+    else if (p3 > 0) {
         wait(&p3);
         int endTime3 = clock_gettime(CLOCK_REALTIME, &end3);
         double runTime3 = (end3.tv_sec + 1.0e-9*end3.tv_nsec - (start3.tv_sec + 1.0e-9*start3.tv_nsec));
         printf("Runtime of Process 3 = %lfs\n", runTime3);
-
     }
-
-    
-
     return 0;
 }

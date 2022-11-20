0f0e534b46e1cbfdc4cea7ccb8d63379d470b98d
bl0nder <goelpalaash@gmail.com>
2022-11-20 11:50:50 +0530

    downloading kernel and config file instead of copying it

--- compileKernel1.sh
+++ compileKernel1.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 mkdir -p kernel1
 cd kernel1
-cp ../linux-5.19.9.tar .
+wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.19.9.tar.xz
+unxz linux-5.19.9.tar.xz
 tar -xvf linux-5.19.9.tar
 cd linux-5.19.9
 make mrproper
-cp ../../config-rev-9-gold .config
+wget https://github.com/bl0nder/ThreadScheduling/blob/405cc153b9dc8fd333485780413fb1de3d3b7e09/config-rev-9-gold
 make

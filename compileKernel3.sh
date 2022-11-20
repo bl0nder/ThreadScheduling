10455628d20032084db5b648a0c673ba3533fb0a
bl0nder <goelpalaash@gmail.com>
2022-11-20 16:35:59 +0530

    test

--- compileKernel3.sh
+++ compileKernel3.sh
@@ -3,8 +3,8 @@ mkdir -p kernel3
 cd kernel3
 wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.19.9.tar.xz
 unxz linux-5.19.9.tar.xz
-tar -xvf linux-5.19.9.tar
-cd linux-5.19.9
-make mrproper
-wget -O .config https://raw.githubusercontent.com/bl0nder/ThreadScheduling/main/config-rev-9-gold
+# tar -xvf linux-5.19.9.tar
+# cd linux-5.19.9
+# make mrproper
+# wget -O .config https://raw.githubusercontent.com/bl0nder/ThreadScheduling/main/config-rev-9-gold
 # make

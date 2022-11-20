bd6c1aee676a558afc15d0ca0c493f9a809b85fd
bl0nder <goelpalaash@gmail.com>
2022-11-20 13:06:32 +0530

    .

--- processSched.c
+++ processSched.c
@@ -41,7 +41,7 @@ int main() {
     // p1 = fork();
 
     if ((p1 = fork()) == 0) {
-        sched_setscheduler(p1, SCHED_OTHER, param1);
+        sched_setscheduler(p1, SCHED_FIFO, param1);
         printf("Process 1 started\n");
         for (int i=1; i<=count; i++) {
             continue;
@@ -61,7 +61,7 @@ int main() {
     }
     
     else if ((p3 = fork()) == 0) {
-        sched_setscheduler(p3, SCHED_FIFO, param3);
+        sched_setscheduler(p3, SCHED_OTHER, param3);
         printf("Process 3 started\n");
         for (int i=1; i<=count; i++) {
             continue;

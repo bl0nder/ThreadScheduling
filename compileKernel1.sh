c202b3d43955a66fe87c3e4f45a742ed70456f38
bl0nder <goelpalaash@gmail.com>
2022-11-20 15:06:37 +0530

    hello world

--- compileKernel1.sh
+++ compileKernel1.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 mkdir -p kernel1
 cd kernel1
-echo hello world 1
-# wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.19.9.tar.xz
-# unxz linux-5.19.9.tar.xz
-# tar -xvf linux-5.19.9.tar
-# cd linux-5.19.9
-# make mrproper
-# wget https://raw.githubusercontent.com/bl0nder/ThreadScheduling/main/config-rev-9-gold
-# make
+wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.19.9.tar.xz
+unxz linux-5.19.9.tar.xz
+tar -xvf linux-5.19.9.tar
+cd linux-5.19.9
+make mrproper
+wget https://raw.githubusercontent.com/bl0nder/ThreadScheduling/main/config-rev-9-gold
+make
--- compileKernel2.sh
+++ compileKernel2.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 mkdir -p kernel2
 cd kernel2
-echo hello world 2
-# wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.19.9.tar.xz
-# unxz linux-5.19.9.tar.xz
-# tar -xvf linux-5.19.9.tar
-# cd linux-5.19.9
-# make mrproper
-# wget https://raw.githubusercontent.com/bl0nder/ThreadScheduling/main/config-rev-9-gold
-# make
+wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.19.9.tar.xz
+unxz linux-5.19.9.tar.xz
+tar -xvf linux-5.19.9.tar
+cd linux-5.19.9
+make mrproper
+wget https://raw.githubusercontent.com/bl0nder/ThreadScheduling/main/config-rev-9-gold
+make

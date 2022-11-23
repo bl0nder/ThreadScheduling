8fdea8da825be4e343c5dd80fb24a987092e701b
bl0nder <goelpalaash@gmail.com>
2022-11-24 00:27:08 +0530

    i know this will not work

--- kernel_2d_memcpy.c
+++ kernel_2d_memcpy.c
@@ -3,15 +3,13 @@
 #include <linux/kernel.h>
 #include <linux/syscalls.h>
 
-int main () {
-    // int matrix[5][5] = {{1,2,3,4,5}, {6,7,8,9,10}, {11,12,13,14,15}, {16,17,18,19,20}, {21,22,23,24,25}};
-    // int matrixCopy[5][5];
+SYSCALL_DEFINE0 (kernel_2d_memcpy) {
+    int matrix[5][5] = {{1,2,3,4,5}, {6,7,8,9,10}, {11,12,13,14,15}, {16,17,18,19,20}, {21,22,23,24,25}};
+    int tmp[5][5];
+    int matrixCopy[5][5];
 
-    int matrix = 50;
-    int matrixCopy;
+    copy_from_user((void *) &tmp, (const void*) &matrix, 25*sizeof(int));
+    copy_to_user((void *) &matrixCopy, (const void*) &tmp, 25*sizeof(int));
 
-    copy_from_user((void *) &matrixCopy, (const void*) &matrix, 1*sizeof(int));
-
-    printf("matrixCopy: %d\n", matrixCopy);
     return 0;
 }

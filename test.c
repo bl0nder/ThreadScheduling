42ff70d9cea3c7a1533573393c7b106dec33f3e3
bl0nder <goelpalaash@gmail.com>
2022-11-24 01:56:17 +0530

    .

--- test.c
+++ test.c
@@ -10,6 +10,14 @@ int main() {
     int sourceMatrix[5][5] = {{1,2,3,4,5}, {6,7,8,9,10}, {11,12,13,14,15}, {16,17,18,19,20}, {21,22,23,24,25}};
     int destMatrix[5][5] = {{0,0,0,0,0}, {0,0,0,0,0}, {0,0,0,0,0}, {0,0,0,0,0}, {0,0,0,0,0}};
 
+    printf("Here is the destination matrix before the syscall has been invoked:\n");
+    for (int i=0; i<5; i++) {
+        for (int j=0; j<5; j++) {
+            printf("%d ", destMatrix[i][j]);
+        }
+        printf("\n");
+    }
+
     int flag = syscall(451, sourceMatrix, destMatrix, 5, 5);
 
     if (!flag) {
